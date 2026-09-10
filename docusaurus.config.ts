4f0badd1d36c4e306cd896097e6dbf9f911d97fd
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2026-09-10 11:45:00 +0200

    Update site URL in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: process.env.DEPLOYMENT_URL ?? "https://belsamor.github.io/",
+  url: process.env.DEPLOYMENT_URL ?? "https://mihaiandreineacsu.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.BASE_URL ?? "/",
